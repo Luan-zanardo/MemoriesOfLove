cf6209abd94b50813a6d6e7756bb4248d9531208
Luan Zuchinali Zanardo <luanzanardo2021@outlook.com>
2025-10-30 17:50:27 -0300

    timeline changes

--- girlfriend/src/app/Components/FiltersBar.tsx
+++ girlfriend/src/app/Components/FiltersBar.tsx
@@ -14,13 +14,13 @@ export default function FiltersBar({
   clearFilters,
 }: Props) {
   return (
-    <div className="flex flex-wrap gap-3 mb-4 justify-center items-center">
+    <div className="flex flex-col sm:flex-row gap-3 mb-4 justify-center items-center">
       
       {/* Select */}
       <select
         value={sortOrder}
         onChange={(e) => setSortOrder(e.target.value as "recent" | "oldest")}
-        className="w-48 h-12 px-3 rounded-lg border border-pink-300 bg-pink-200 text-gray-700 focus:outline-none focus:ring-2 focus:ring-pink-400 appearance-none"
+        className="w-full sm:w-48 h-12 px-3 rounded-lg border border-pink-300 bg-pink-50 text-gray-700 focus:outline-none focus:ring-2 focus:ring-pink-400 shadow-sm transition"
       >
         <option value="recent">Mais recentes</option>
         <option value="oldest">Mais antigos</option>
@@ -31,13 +31,13 @@ export default function FiltersBar({
         type="date"
         value={filterDate}
         onChange={(e) => setFilterDate(e.target.value)}
-        className="w-48 h-12 px-3 rounded-lg border border-pink-300 bg-pink-200 text-gray-700 focus:outline-none focus:ring-2 focus:ring-pink-400"
+        className="w-full sm:w-48 h-12 px-3 rounded-lg border border-pink-300 bg-pink-50 text-gray-700 focus:outline-none focus:ring-2 focus:ring-pink-400 shadow-sm transition"
       />
 
       {/* Botão */}
       <button
         onClick={clearFilters}
-        className="w-48 h-12 px-3 bg-pink-400 hover:bg-pink-500 text-white font-semibold rounded-lg transition"
+        className="w-full sm:w-48 h-12 px-3 bg-pink-200 hover:bg-pink-100 text-pink-400 font-semibold rounded-lg shadow-sm transition"
       >
         Limpar filtros
       </button>
